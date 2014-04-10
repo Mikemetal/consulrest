58b8f8b1b70a65066227b010e08028064d133cf5
Mike <miguelbq88@gmail.com>
2014-04-09 23:12:15 -0700

    Archivo admin.py

--- consul/admin.py
+++ consul/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from agenda.models import Agenda
+from paciente.models import Paciente
 
-# Register your models here.
+class AgendaAdmin(admin.ModelAdmin):
+    list_display = ('Descripcion', 'Fecha', 'HoraInicio', 'HoraFinal','Asistio','Cancelada')
+  #  filter_vertical = ('Fecha',)
+ #   actions = [export_as_csv]
+ #   inlines = [EnlaceInline]
+
+class PacienteAdmin(admin.ModelAdmin):
+    list_display = ('Nombre', 'FechaCreacion')
+  #  filter_vertical = ('Nombre',)
+
+admin.site.register(Agenda,AgendaAdmin)
+admin.site.register(Paciente,PacienteAdmin)
